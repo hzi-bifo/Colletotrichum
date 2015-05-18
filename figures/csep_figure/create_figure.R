e4644b30d6bf004d0049638b79ec52916663216c
philippmuench <muench@uni-duesseldorf.de>
2015-05-18 15:25:13 +0200

    solved bug

--- figures/csep_figure/create_figure.R
+++ figures/csep_figure/create_figure.R
@@ -18,6 +18,11 @@ create_figure <- function(patho_set,
   eff_a$type <- "C*"
   mean_a <- sum(as.numeric(as.matrix(eff_a$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_a$sum_pS)), na.rm=T)
   
+  # non-effector for background
+ # rest_a <- df_a[which(df_a$eff_new != T),]
+#  rest_a <- rest_a[with(rest_a, order(fdr,-ratio)), ]
+#  rest_a$type <- "C* non-effector"
+  
   # second set
   df_b <- as.data.frame(nonpatho_set)
   df_b$fdr <- as.numeric(as.matrix(df_b$fdr))
@@ -26,6 +31,11 @@ create_figure <- function(patho_set,
   eff_b <- eff_b[with(eff_b, order(fdr,-ratio)), ]
   eff_b$type <- "Ct"
   mean_b <- sum(as.numeric(as.matrix(eff_b$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_b$sum_pS)), na.rm=T)
+
+  # non-effector for background
+#  rest_b <- df_a[which(df_b$eff_new != T),]
+#  rest_b <- rest_b[with(rest_b, order(fdr,-ratio)), ]
+#  rest_b$type <- "Ct non-effector"
   
   # check for unique in patho_set
   eff_a$unique <- FALSE
@@ -49,11 +59,19 @@ create_figure <- function(patho_set,
   }
   
   df_both <- rbind(eff_a, eff_b)
-  df_both[which(df_both$type =="C*" & df_both$unique==T),]$type <- "C* unique"
-  df_both[which(df_both$type =="C*" & df_both$unique!=T),]$type <- "C* non-unique"
-  df_both[which(df_both$type =="Ct" & df_both$unique==T),]$type <- "Ct unique"
-  df_both[which(df_both$type =="Ct" & df_both$unique!=T),]$type <- "Ct non-unique"
-
+  if(length(df_both[which(df_both$type =="C*" & df_both$unique==T),]$type) > 0){
+    df_both[which(df_both$type =="C*" & df_both$unique==T),]$type <- "C* unique"
+  }
+  if(length(df_both[which(df_both$type =="C*" & df_both$unique!=T),]$type) > 0){  
+    df_both[which(df_both$type =="C*" & df_both$unique!=T),]$type <- "C* non-unique"
+  }
+  if(length(df_both[which(df_both$type =="Ct" & df_both$unique==T),]$type) > 0){
+    df_both[which(df_both$type =="Ct" & df_both$unique==T),]$type <- "Ct unique" 
+  }  
+  if(length(df_both[which(df_both$type =="Ct" & df_both$unique!=T),]$type) > 0){
+    df_both[which(df_both$type =="Ct" & df_both$unique!=T),]$type <- "Ct non-unique"
+  }
+   # df_both <- rbind(df_both, rest_a, rest_b)
   d <- ggplot(df_both, aes(x= ratio,y=fdr, colour=type))
   d <- d + geom_point()  + theme_bw() + scale_y_log10()
   d <- d +  xlab("dN/dS ratio") + ylab("log10 p-value")
