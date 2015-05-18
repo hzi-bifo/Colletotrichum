bc547fcf95671a2978a1a92a3e061a70371133a1
philippmuench <muench@uni-duesseldorf.de>
2015-05-18 16:16:10 +0200

    changed color for non-uniqe/unique effectors

--- figures/csep_figure/create_figure.R
+++ figures/csep_figure/create_figure.R
@@ -16,7 +16,7 @@ create_figure <- function(patho_set,
   eff_a <- df_a[which(df_a$eff_new == T),]
   eff_a <- eff_a[with(eff_a, order(fdr,-ratio)), ]
   eff_a$sample <- "C*"
-  eff_a$type <- "effector"
+  eff_a$type <- "non-unique effector"
     mean_a <- sum(as.numeric(as.matrix(eff_a$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_a$sum_pS)), na.rm=T)
   
   # non-effector for background
@@ -32,7 +32,7 @@ create_figure <- function(patho_set,
   df_b$ratio <- as.numeric(as.matrix(df_b$ratio))
   eff_b <- df_b[which(df_b$eff_new == T),]
   eff_b <- eff_b[with(eff_b, order(fdr,-ratio)), ]
-  eff_b$type <- "effector"
+  eff_b$type <- "non-unique effector"
   eff_b$sample <- "Ct"
   mean_b <- sum(as.numeric(as.matrix(eff_b$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_b$sum_pS)), na.rm=T)
 
@@ -66,10 +66,12 @@ create_figure <- function(patho_set,
   
   df_both_eff <- rbind(eff_a, eff_b)
   df_both_rest <- rbind(rest_a, rest_b)
- 
-  d <- ggplot(df_both_eff, aes(x= ratio,y=fdr, colour=type, shape=unique))
-  d <- d + geom_point(data=df_both_rest, alpha=0.1)
-  d <- d + geom_point(alpha=0.4)  + theme_bw() + scale_y_log10()
+  
+  df_both_eff[which(df_both_eff$type =="non-unique effector" & df_both_eff$unique==T),]$type <- "unique effector"
+
+  d <- ggplot(df_both_eff, aes(x= ratio,y=fdr, colour=type))
+  d <- d + geom_point(data=df_both_rest, alpha=0.1, color="black")
+  d <- d + geom_point(alpha=0.8)  + theme_bw() + scale_y_log10()
   d <- d + xlab("dN/dS ratio") + ylab("log10 p-value")
   d <- d + facet_grid(. ~ sample)
   d <- d + geom_hline(yintercept=0.01, alpha=0.2)
