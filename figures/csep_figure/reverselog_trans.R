7c80619a3e512970e2a516c96f3c1623c14ed213
philippmuench <philipp.munech@helmholtz-hzi.de>
2015-07-03 14:50:01 +0200

    deleted old folders

--- figures/csep_figure/reverselog_trans.R
+++
@@ -1,7 +0,0 @@
-reverselog_trans <- function(base=exp(1)){
-  trans <- function(x) -log(x, base)
-  inv <- function(x) base^(-x)
-  trans_new(paste0("reverselog-", format(base)), trans, inv,
-            log_breaks(base= base),
-            domain=c(1e-100, Inf))
-}
