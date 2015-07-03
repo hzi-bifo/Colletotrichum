ff6723310bc5b7a659fb31ec184673559d34218e
philippmuench <philipp.munech@helmholtz-hzi.de>
2015-07-03 14:48:58 +0200

    changes on folder structure

---
+++ figures/fig_3b/create_figure.R
@@ -0,0 +1,89 @@
+#' This functions creates CSEP figure
+#' @author Philipp C. Münch, philipp.muench@helmholtz-hzi.de
+#' @param 
+#' @keywords dnds
+#' @export
+#' @examples 
+#' 
+create_figure <- function(patho_set=c_patho,
+                        nonpatho_set=c_t,
+                        fuNOG_annotation=annotation){
+  require(ggplot2)
+  require(scales)
+  source("reverselog_trans.R")
+  # first set
+  df_a <- as.data.frame(patho_set)
+  df_a$fdr <- as.numeric(as.matrix(df_a$fdr))
+  df_a$ratio <- as.numeric(as.matrix(df_a$ratio))
+  eff_a <- df_a[which(df_a$eff_new == T),]
+  eff_a <- eff_a[with(eff_a, order(fdr,-ratio)), ]
+  eff_a$sample <- "C*"
+  eff_a$type <- "non-unique effector"
+    mean_a <- sum(as.numeric(as.matrix(eff_a$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_a$sum_pS)), na.rm=T)
+  
+  # non-effector for background
+  rest_a <- df_a[which(df_a$eff_new != T),]
+  rest_a <- rest_a[with(rest_a, order(fdr,-ratio)), ]
+  rest_a$type <- "non-effector"
+  rest_a$sample <- "C*"
+  rest_a$unique <- F
+  
+  # second set
+  df_b <- as.data.frame(nonpatho_set)
+  df_b$fdr <- as.numeric(as.matrix(df_b$fdr))
+  df_b$ratio <- as.numeric(as.matrix(df_b$ratio))
+  eff_b <- df_b[which(df_b$eff_new == T),]
+  eff_b <- eff_b[with(eff_b, order(fdr,-ratio)), ]
+  eff_b$type <- "non-unique effector"
+  eff_b$sample <- "Ct"
+  mean_b <- sum(as.numeric(as.matrix(eff_b$sum_pN)), na.rm=T)/sum(as.numeric(as.matrix(eff_b$sum_pS)), na.rm=T)
+
+  # non-effector for background
+  rest_b <- df_a[which(df_b$eff_new != T),]
+  rest_b <- rest_b[with(rest_b, order(fdr,-ratio)), ]
+  rest_b$type <- "non-effector"
+  rest_b$sample <- "Ct"
+  rest_b$unique <- F
+  
+  # check for unique in patho_set
+  eff_a$unique <- FALSE
+  i <- 1
+  for (element_name in eff_a$name) {
+    elem_match <- match(as.character(as.matrix(element_name)), eff_b$name)
+    if(is.na(elem_match)){
+      eff_a$unique[i] <- TRUE
+    }
+    i <- i + 1 
+  }
+  # check for unique in patho_set
+  eff_b$unique <- FALSE
+  i <- 1
+  for (element_name in eff_a$name) {
+    elem_match <- match(as.character(as.matrix(element_name)), eff_a$name)
+    if(is.na(elem_match)){
+      eff_b$unique[i] <- TRUE
+    }
+    i <- i + 1 
+  }
+  
+  df_both_eff <- rbind(eff_a, eff_b)
+  df_both_rest <- rbind(rest_a, rest_b)
+  
+  df_both_eff[which(df_both_eff$type =="non-unique effector" & df_both_eff$unique==T),]$type <- "unique effector"
+
+  d <- ggplot(df_both_eff, aes(x= fdr,y=ratio, colour=type))
+  d <- d + geom_point(data=df_both_rest, alpha=0.1, color="grey70") 
+  d <- d + geom_point(alpha=0.8)  + theme_bw() #+ scale_x_log10(limits=(c(1,1*10^(-68))))
+  d <- d + scale_x_continuous(trans=reverselog_trans(10), limits=c(1,1*10^(-68)))
+  d <- d + ylab("dN/dS ratio") + xlab("log10 p-value") 
+  d <- d + facet_grid(. ~ sample)
+  d <- d + geom_vline(yintercept=0.01, alpha=0.2) 
+  d <- d + scale_y_continuous(breaks=c(0,0.2,0.4,0.6,0.8,1))
+  d <- d + theme(strip.background = element_rect(color="white", fill="white"),
+                 text = element_text(size=18))
+#  d <- d + geom_rug(data=df_both_rest, col=rgb(.5,0,0,alpha=0.01))
+#  d <- d + geom_rug(data=df_both_rest, alpha=0.1, color="grey70")
+#d <- d+ geom_rug(alpha=0.2)
+
+  return(d)
+}
