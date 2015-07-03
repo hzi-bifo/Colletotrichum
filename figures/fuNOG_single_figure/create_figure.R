3b2604ad595536a07d00fdde7cde0920b857e046
philippmuench <philipp.munech@helmholtz-hzi.de>
2015-07-03 14:00:06 +0200

    sorted by median

--- figures/fuNOG_single_figure/create_figure.R
+++ figures/fuNOG_single_figure/create_figure.R
@@ -4,7 +4,6 @@
 #' @author Philipp C. Münch philipp.muench@helmholtz-hzi.de
 #' @param set A dataset with the columns name ratio, desc, sum_pN, sum_pS, fdr, pvalue, stars
 #' @param fuNOG_annotation A path to fuNOG annotation file with columns gfam and fuNOG. 
-#' This file are not filtered by accuracy so it must filtered before
 #' @param fuNOG_lvl A character with fuNOG lvl of interest. e.g. "lvl1", or "lvl2"
 #' @param remove Logical, if ture the fuNOG annotations "NA" and "
 #' @keywords dnds
@@ -13,7 +12,6 @@ create_figure <- function(set,
                                   fuNOG_lvl="lvl2", 
                                   remove=F,
                                   custom_annotation=T,
-                                  new_eff=T,
                                   sec_path="data/secreted_genes_v_gfam.txt",
                                   eff_path="data/csep_genes_v_gfam.txt"){
   require(ggplot2)
@@ -75,76 +73,17 @@ create_figure <- function(set,
   }
   
   # create plot 
-  d <- ggplot(df, aes(x= reorder(lvl, -ratio, median, order=TRUE),y=ratio, fill=subset))
+  d <- ggplot(df, aes(x= reorder(lvl, as.numeric(as.matrix(ratio)), median, order=TRUE),y=ratio, fill=subset))
   #d <- ggplot(df, aes(factor(lvl),ratio, fill=subset))
   d <- d + geom_boxplot( outlier.size = 1) #+ geom_text(data = label.df, label = "***")
+  
+ # d <- d + geom_point(aes(color=subset), alpha=0.1)
   d <- d + theme_bw() + coord_flip()      
-  if(new_eff){
-    d <- d + ggtitle("New sec/eff annotation")
-  }
   
   d <- d + theme(plot.title = element_text(size=15, vjust=1, lineheight=0.6))
   d <- d + ggtitle(paste("fuNOG", fuNOG_lvl))
   d <- d + xlab(paste("fuNOG categories on", fuNOG_lvl)) +  ylab("ratio")
   
-  
-  # test
-  
-  # test CSEP(CPAS) vs all(CPAS)
-#  case_pN <-sum(as.numeric(as.matrix(df[which(df$lvl=="Cellular processes and signaling" & df$subset=="CSEP"),]$sum_pN)))
-#  case_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Cellular processes and signaling" & df$subset=="CSEP"),]$sum_pS)))
-  
-#  all_pN <- sum(as.numeric(as.matrix(df[which(df$lvl=="Cellular processes and signaling" & df$subset=="all"),]$sum_pN)))
-#  all_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Cellular processes and signaling" & df$subset=="all"),]$sum_pS)))
-
-#  test_mat <- matrix(c(case_pN, case_pS, 
-#                       all_pN,all_pS),
-#                     nrow = 2)
-  
- # test_case <- fisher.test(test_mat, alternative = "greater")
-  
-  # test CSEP(INFO) vs all(INFO)
-#  case_pN <-sum(as.numeric(as.matrix(df[which(df$lvl=="Information storage and processing" & df$subset=="CSEP"),]$sum_pN)))
-#  case_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Information storage and precessing" & df$subset=="CSEP"),]$sum_pS)))
-  
-#  all_pN <- sum(as.numeric(as.matrix(df[which(df$lvl=="Information storage and precessing" & df$subset=="all"),]$sum_pN)))
-#  all_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Information storage and precessing" & df$subset=="all"),]$sum_pS)))
-  
-#  test_mat <- matrix(c(case_pN, case_pS, 
-#                       all_pN,all_pS),
-#                     nrow = 2)
-  
-#  test_case <- fisher.test(test_mat, alternative = "greater")
-
-
-# test CSEP(defense) vs all(defense)
-  #case_pN <-sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="CSEP"),]$sum_pN)))
-  #case_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="CSEP"),]$sum_pS)))
-
- # all_pN <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="all"),]$sum_pN)))
-#  all_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="all"),]$sum_pS)))
-
-#  test_mat <- matrix(c(case_pN, case_pS, 
-#                       all_pN,all_pS),
-#                     nrow = 2)
-
- # test_case <- fisher.test(test_mat, alternative = "greater")
-
-
-
-# test CSEP(turnover) vs all(turnover)
-#case_pN <-sum(as.numeric(as.matrix(df[which(df$lvl=="Posttranslational modification, protein turnover, chaperones" & df$subset=="CSEP"),]$sum_pN)))
-#case_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="CSEP"),]$sum_pS)))
-
-#all_pN <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="all"),]$sum_pN)))
-#all_pS <- sum(as.numeric(as.matrix(df[which(df$lvl=="Defense mechanisms" & df$subset=="all"),]$sum_pS)))#
-#
-#test_mat <- matrix(c(case_pN, case_pS, 
-#                     all_pN,all_pS),
-#                   nrow = 2)
-#
-#test_case <- fisher.test(test_mat, alternative = "greater")
-
   return(d)  
 }
 
