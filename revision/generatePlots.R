65545a983f76b4d117f53573c95c882a6c3f4fea
philippmuench <philipp.munech@helmholtz-hzi.de>
2015-12-08 15:20:09 +0100

    add annotated table

--- revision/generatePlots.R
+++ revision/generatePlots.R
@@ -26,21 +26,32 @@ result_mat[csep_match[!is.na(csep_match)],]$type <- "CSEP"
 
 
 # fisher test
-testmat <- matrix(c(sum(result_csep$sum_pN, na.rm=T), sum(result_non_csep$sum_pN, na.rm=T), sum(result_csep$sum_pS, na.rm=T), sum(result_non_csep$sum_pS, na.rm=T)),
+# testmat <- matrix(c(sum(result_csep$sum_pN, na.rm=T), sum(result_non_csep$sum_pN, na.rm=T), sum(result_csep$sum_pS, na.rm=T), sum(result_non_csep$sum_pS, na.rm=T)),
                     nrow = 2, dimnames = list(c("CSEP", "non-CSEP"),c("sumDn", "sumDs")))
 
 fisher.test(testmat, alternative = "greater") # p-value < 2.2e-16, OR=2.128149
+
+# add annotation
+suptable$gfam <- "NA"
+suptable <- read.table("data/annotation/csep.txt",se=";", header=T)
+annottable <- read.table("data/annotation/gfam2id.txt", sep=";")
+supmatch <- match(as.character(as.matrix(suptable$GeneID)), as.character(as.matrix(annottable$V2)))
+suptable$gfam<- annottable[supmatch,]$V1
+
+supmatch2 <- match(as.character(as.matrix(suptable$gfam)), as.character(as.matrix(result_mat)))
+suptable$dN.dS <- result_mat[supmatch2,]$ratio
+write.table(suptable, file = "annotated_table.txt", quote=F, row.names=F, sep=";")
 
 # start plot
 d <- ggplot(result_mat, aes(x=fdr,y=ratio, colour=type))
 
 # scale und theme
-d <- d + scale_x_continuous(trans=reverselog_trans(10), limits=c(1,1*10^(-68)))
+d <- d + scale_x_continuous(trans=reverselog_trans(10), limits=c(1,1*10^(-168)))
 d <- d + geom_point(alpha=0.4)  + theme_bw() 
 
 
 e <- ggplot(result_mat, aes(x=type, y=ratio, colour=type)) + theme_bw() + scale_y_log10()
-e <- e + geom_violin(trim=FALSE, fill='#A4A4A4', color="darkred") + geom_boxplot(notch = TRUE, outlier.colour = "black", outlier.size = 1) #+ geom_jitter(alpha=0.01)
+e <- e + geom_boxplot(notch = TRUE, outlier.colour = "black", outlier.size = 1) #+ geom_jitter(alpha=0.01)
 
 e <- ggplot(result_mat, aes(x=type, y=ratio, colour=type, fill=type)) + theme_bw() + scale_y_log10() + theme_minimal()
 e <- e + geom_violin(trim=FALSE, fill="gray90", colour="black") +  geom_boxplot(width=0.1, outlier.colour = "darkred", outlier.size = 1, colour="black", fill="grey30")  #+ geom_jitter(alpha=0.01)
